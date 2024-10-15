9a02d0ef972427159f9a7557a861d7b5d5e424a0
leichtje <113708612+leichtje@users.noreply.github.com>
2024-10-15 11:11:30 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Server_Application_Final_Project.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
