9025ec9c3a969de9bdf9fbf5e55c73d3df254de7
leichtje <113708612+leichtje@users.noreply.github.com>
2024-11-30 19:58:50 -0500

    DB Test 3

--- Migrations/20241201005637_Update 1.Designer.cs
+++ Migrations/20241201005637_Update 1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Web_Server_Application_Final_Project.Models;
 
@@ -11,9 +12,11 @@ using Web_Server_Application_Final_Project.Models;
 namespace Web_Server_Application_Final_Project.Migrations
 {
     [DbContext(typeof(FinalContext))]
-    partial class FinalContextModelSnapshot : ModelSnapshot
+    [Migration("20241201005637_Update 1")]
+    partial class Update1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241201005637_Update 1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Server_Application_Final_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cameras",
+                keyColumn: "CameraId",
+                keyValue: 1,
+                columns: new[] { "Name", "Year" },
+                values: new object[] { "Nikon Z6", 2018 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cameras",
+                keyColumn: "CameraId",
+                keyValue: 1,
+                columns: new[] { "Name", "Year" },
+                values: new object[] { "Nikon z6", 1959 });
+        }
+    }
+}
